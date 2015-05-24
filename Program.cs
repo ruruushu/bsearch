8a1d070f9b171d3ad84c382fb62e04b765955383
djelusic <djelusic133@gmail.com>
2015-05-24 23:27:45 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bsearch
+{
+    static class Search
+    {
+        private static int helper(int[] arr, int val, int left, int right)
+        {
+            if(right < left) 
+                return -1;
+
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == val) return mid;
+            else if (arr[mid] < val) return helper(arr, val, mid + 1, right);
+            else return helper(arr, val, left, mid - 1);
+        }
+
+        public static int bsearch(int[] arr, int val)
+        {
+            Console.WriteLine("test2");
+            int left = 0, right = arr.Length - 1;
+            return helper(arr, val, left, right);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+            Console.WriteLine("{0}", Search.bsearch(arr, 7));
+            Console.WriteLine("test");
+        } 
+    }
+}
